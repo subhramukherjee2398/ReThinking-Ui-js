7ffc8565b98ea2410a017a40ffc59e197a5927a2
Subhra Mukherjee <subhramukherjee560@gmail.com>
2022-12-27 20:03:18 +0530

    End

--- JS/j8.js
+++ JS/j8.js
@@ -19,3 +19,12 @@ console.log(Object.hasOwn(User,'name'))
 //4. in 
 
 console.log('name' in User)
+
+//Q2. Difference between const and Object freeze
+
+//ans :
+
+/*
+ if we declare object as const it can not be assignment but we can still update it's value
+ but for object freeze - ( follow it's rule )
+*/
